75e28aff643a658980ca1928bd78e4d157803422
William Durand <will+git@drnd.me>
2021-01-15 21:14:17 +0100

    Refactor index.js and add pause feature

--- web/index.js
+++ web/index.js
@@ -6,121 +6,159 @@ import { disassemble } from "./disassembler";
 import { makeKeypad } from "./keypad";
 import { hexformat } from "./utils";
 
-// TODO: retrieve these values via the interpreter instance.
-const WIDTH = 64;
-const HEIGHT = 32;
-// TODO: make it configurable.
-const DEFAULT_SPEED = 9;
+const Chip8 = {
+  // TODO: retrieve these values via the interpreter instance.
+  WIDTH: 64,
+  HEIGHT: 32,
+  // TODO: make it configurable.
+  speed: 9,
+  paused: false,
 
-const $display = document.getElementById("display-canvas");
-const $opcode = document.querySelector(".opcode .values");
-const $registers1 = document.querySelector(".registers .values-1");
-const $registers2 = document.querySelector(".registers .values-2");
+  display: null,
+  audio: null,
 
-// TODO: add controls to mute sound
-const audio = createAudio();
-// TODO: add controls to change display dimensions
-const display = createDisplay(
-  $display,
-  WIDTH,
-  HEIGHT,
-  screen.width,
-  screen.height
-);
+  keysPressed: {},
 
-const keysPressed = {};
+  $pauseBtn: null,
 
-document.addEventListener("keydown", (event) => {
-  keysPressed[event.key] = true;
-});
+  init(_document, _screen) {
+    // TODO: add controls to change display dimensions
+    this.display = createDisplay(
+      _document.getElementById("display-canvas"),
+      this.WIDTH,
+      this.HEIGHT,
+      _screen.width,
+      _screen.height
+    );
 
-document.addEventListener("keyup", (event) => {
-  keysPressed[event.key] = false;
-});
+    // TODO: add controls to mute sound
+    this.audio = createAudio();
 
-const run = (rom, speed) => {
-  const interpreter = new libchipolata.JsInterpreter(rom);
+    this.$pauseBtn = _document.querySelector("#btn-pause");
 
-  const vram = new Uint8Array(
-    memory.buffer,
-    interpreter.get_vram_ptr(),
-    WIDTH * HEIGHT
-  );
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onPauseClick = this.onPauseClick.bind(this);
 
-  const ram = new Uint8Array(memory.buffer, interpreter.get_ram_ptr(), 0x1000);
-  $opcode.innerHTML = disassemble(ram);
+    _document.addEventListener("keydown", this.onKeyDown);
+    _document.addEventListener("keyup", this.onKeyUp);
+    this.$pauseBtn.addEventListener("click", this.onPauseClick);
+  },
 
-  const v_registers = new Uint8Array(
-    memory.buffer,
-    interpreter.get_v_registers_ptr(),
-    16
-  );
+  onKeyDown(event) {
+    this.keysPressed[event.key] = true;
+  },
 
-  const updateInfo = () => {
-    const pc = interpreter.get_pc();
+  onKeyUp(event) {
+    this.keysPressed[event.key] = false;
+  },
 
-    const className = "current-addr";
-    const oldAddr = document.querySelector(`.${className}`);
-    oldAddr && oldAddr.classList.toggle(className);
+  onPauseClick() {
+    this.$pauseBtn.classList.toggle("btn-ghost");
+    this.$pauseBtn.textContent = this.paused ? "stop" : "start";
+    this.paused = !this.paused;
+  },
 
-    const newAddr = document.querySelector(`.addr-${pc}`);
-    if (newAddr) {
-      newAddr.classList.toggle(className);
-      newAddr.parentElement.scrollTo(
-        0,
-        newAddr.offsetTop - newAddr.parentElement.offsetTop - 70
-      );
-    }
+  run(rom) {
+    const interpreter = new libchipolata.JsInterpreter(rom);
 
-    const values1 = [];
-    const values2 = [];
-    for (let i = 0; i < 8; i++) {
-      values1.push(
-        `v${String(i).padStart(2, "0")}=${hexformat(v_registers[i], 2)}`
-      );
-      values2.push(
-        `v${String(i + 8).padStart(2, "0")}=${hexformat(v_registers[i + 8], 2)}`
-      );
-    }
+    const vram = new Uint8Array(
+      memory.buffer,
+      interpreter.get_vram_ptr(),
+      this.WIDTH * this.HEIGHT
+    );
 
-    $registers1.innerHTML = values1.join("<br>");
-    $registers2.innerHTML = values2.join("<br>");
-  };
+    const ram = new Uint8Array(
+      memory.buffer,
+      interpreter.get_ram_ptr(),
+      0x1000
+    );
 
-  const renderLoop = () => {
-    let redraw = false;
-    for (let i = 0; i < speed; i++) {
-      interpreter.update_keypad(makeKeypad(keysPressed));
-      interpreter.step();
+    const $opcode = document.querySelector(".opcode .values");
+    $opcode.innerHTML = disassemble(ram);
 
-      if (interpreter.should_redraw()) {
-        redraw = true;
+    const v_registers = new Uint8Array(
+      memory.buffer,
+      interpreter.get_v_registers_ptr(),
+      16
+    );
+
+    const $registers1 = document.querySelector(".registers .values-1");
+    const $registers2 = document.querySelector(".registers .values-2");
+
+    const updateInfo = () => {
+      const pc = interpreter.get_pc();
+
+      const className = "current-addr";
+      const oldAddr = document.querySelector(`.${className}`);
+      oldAddr && oldAddr.classList.toggle(className);
+
+      const newAddr = document.querySelector(`.addr-${pc}`);
+      if (newAddr) {
+        newAddr.classList.toggle(className);
+        newAddr.parentElement.scrollTo(
+          0,
+          newAddr.offsetTop - newAddr.parentElement.offsetTop - 70
+        );
       }
-    }
 
-    if (redraw) {
-      display.draw(vram);
-    }
+      const values1 = [];
+      const values2 = [];
+      for (let i = 0; i < 8; i++) {
+        values1.push(
+          `v${String(i).padStart(2, "0")}=${hexformat(v_registers[i], 2)}`
+        );
+        values2.push(
+          `v${String(i + 8).padStart(2, "0")}=${hexformat(
+            v_registers[i + 8],
+            2
+          )}`
+        );
+      }
 
-    if (interpreter.should_beep()) {
-      audio.start();
-    } else {
-      audio.stop();
-    }
+      $registers1.innerHTML = values1.join("<br>");
+      $registers2.innerHTML = values2.join("<br>");
+    };
 
-    interpreter.update_timers();
+    const renderLoop = () => {
+      if (!this.paused) {
+        let redraw = false;
+        for (let i = 0; i < this.speed; i++) {
+          interpreter.update_keypad(makeKeypad(this.keysPressed));
+          interpreter.step();
 
-    updateInfo();
+          if (interpreter.should_redraw()) {
+            redraw = true;
+          }
+        }
 
-    requestAnimationFrame(renderLoop);
-  };
+        if (redraw) {
+          this.display.draw(vram);
+        }
 
-  renderLoop();
+        if (interpreter.should_beep()) {
+          this.audio.start();
+        } else {
+          this.audio.stop();
+        }
+
+        interpreter.update_timers();
+
+        updateInfo();
+      }
+
+      requestAnimationFrame(renderLoop);
+    };
+
+    renderLoop();
+  },
 };
+
+Chip8.init(document, screen);
 
 fetch("./space-invaders.ch8").then(async (response) => {
   const buffer = await response.arrayBuffer();
   const rom = new Uint8Array(buffer);
 
-  run(rom, DEFAULT_SPEED);
+  Chip8.run(rom);
 });
